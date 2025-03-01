a377a88838e5a3158b701b55bbbb6d628c227c47
MadLad-prime <azuresky01010@gmail.com>
2025-03-01 20:24:55 +0300

    Update scripts_v4.js

--- scripts_v4.js
+++ scripts_v4.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function loadImages() {
     console.log("Fetching latest images from Cloudinary...");
 
-    const cloudinaryJsonURL = "https://res.cloudinary.com/dujlwpbrv/raw/upload/cloudinary_p9cutd.json";
+    const cloudinaryJsonURL = "https://res.cloudinary.com/dujlwpbrv/raw/upload/cloudinary_ddd1s.json";
 
     fetch(cloudinaryJsonURL + `?timestamp=${new Date().getTime()}`) // Force fresh fetch
         .then(response => {
